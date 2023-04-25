beb20a32c3bc411f3d2dd4b8f007f656bac62bc7
Dom <you@example.com>
2023-04-24 17:03:28 -0700

    quick sort

--- sort.h
+++ sort.h
@@ -27,4 +27,6 @@ void swap_list(listint_t **h, listint_t **a, listint_t *b);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void array_swap(int *array, int a, int b);
+void partition(int *array, int low, int high, size_t size);
 #endif
